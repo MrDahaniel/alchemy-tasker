5badfd18e38fce27d16107d902ee2b750ef20113
Daniel <danieldavid2001@gmail.com>
2020-10-05 14:35:31 -0500

    Fixed User Controller

--- alchemy_tasker/lib/alchemy_tasker_web/controllers/user_controller.ex
+++ alchemy_tasker/lib/alchemy_tasker_web/controllers/user_controller.ex
@@ -1,7 +1,11 @@
 defmodule AlchemyTaskerWeb.UserController do
     user AlchemyTaskerWeb, :controller
 
-    def new(conn, _params) do
+    def new(_conn, _params) do
         :nothing
+    end
+
+    def user(_conn, _params) do
+        
     end
 end
