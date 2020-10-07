9675fdd31d2ddab5a8192da7a1cf25bd32307009
VashLT <mcvicksilvaa@gmail.com>
2020-10-07 16:58:01 -0500

    Added page layout

--- alchemy_tasker/lib/alchemy_tasker_web/controllers/page_controller.ex
+++ alchemy_tasker/lib/alchemy_tasker_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule AlchemyTaskerWeb.PageController do
   use AlchemyTaskerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "alchemy.html")
+    render(conn, "index.html")
   end
 end
